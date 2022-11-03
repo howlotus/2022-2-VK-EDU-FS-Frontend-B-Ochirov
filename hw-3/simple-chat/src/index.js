8236e0873845f03ac2e155fb423476ba280ce83e
Badma Ochirov <howlotus@yandex.ru>
2022-11-03 23:25:40 +0300

    deploy v.1

--- hw-3/simple-chat/src/index.js
+++ hw-3/simple-chat/src/index.js
@@ -2,3 +2,4 @@ import './index.css';
 import './screen1_style.css';
 import './screen2_style.css';
 import './screen1.js';
+import './screen2.js';
---
+++ hw-4/simple-chat/src/screen2.js
@@ -0,0 +1,58 @@
+function toChat(name, seen) {
+    document.getElementById('phone_screen1').style.display = "none";
+    document.getElementById('phone_screen2').style.display = "flex";
+
+    let htmlContent = '';
+    const chats = JSON.parse(localStorage.getItem(name)).chats;
+    for (const obj of chats) {
+        htmlContent += makeMessage(obj);
+    }
+    document.getElementById('dynamic_content').innerHTML = htmlContent;
+
+    document.getElementById('user_name').innerText = name;
+    document.getElementById('user_seen').innerText = seen;
+
+    let input = document.getElementById('message_input');
+    const form = document.getElementById('my_form');
+
+    input.value = "";
+    form.addEventListener('submit', handleSubmit);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const inputValue = input.value;
+        if (inputValue === "")
+            return;
+        input.value = "";
+
+        const name = document.getElementById('user_name').innerText;
+        let fullChat = JSON.parse(localStorage.getItem(name));
+        let array = fullChat.chats;
+        const obj = {
+            id: array.length,
+            class: 2,
+            name: "Я",
+            text: inputValue,
+            time: (new Date()).toLocaleTimeString("ru-Ru").substring(0, 5)
+        };
+
+        array.push(obj);
+        fullChat.chats = array;
+        fullChat.status = true;
+        localStorage.setItem(name, JSON.stringify(fullChat));
+
+        document.getElementById('dynamic_content').insertAdjacentHTML('beforeend', makeMessage(obj));
+    }
+}
+
+function makeMessage(obj) {
+    return `
+                <div class="message message--user-`+ obj.class + `" id="message` + obj.id + `">
+                    <div class="message-text">
+                        <div class="name">` + obj.name + `</div>
+                        <div class="text">` + obj.text + `</div>
+                        <div class="time">` + obj.time + `   <i class="material-icons" id="done">done_all</i></div>
+                    </div>
+                </div>`;
+}
