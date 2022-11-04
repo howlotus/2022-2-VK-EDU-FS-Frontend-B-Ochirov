06b1fdf4447c8c266228cfe2f501403449c90938
Badma Ochirov <howlotus@yandex.ru>
2022-11-05 02:28:27 +0300

    homework-4 is done

--- hw-4/simple-chat/src/chatNotification.js
+++ hw-4/simple-chat/src/chatNotification.js
@@ -3,23 +3,32 @@ import toChatList from "./chatList";
 function notificationUp() {
     let notifications = document.getElementsByClassName('hidden');
 
-    for (let note of notifications) {
-        note.style.display = "flex";
-        note.style.opacity = "1";
+    const f0 = () => {
+        for (let note of notifications) {
+            note.style.display = "flex";
+        }
     }
 
     const f1 = () => {
         for (let note of notifications) {
+            note.style.opacity = "1";
+        }
+    }
+
+    const f2 = () => {
+        for (let note of notifications) {
             note.style.opacity = "0";
         }
     }
-    const f2 = () => {
+    const f3 = () => {
         for (let note of notifications) {
             note.style.display = "none";
         }
     }
-    setTimeout(f1, 4000);
-    setTimeout(f2, 6000);
+    f0();
+    setTimeout(f1, 1000);
+    setTimeout(f2, 5000);
+    setTimeout(f3, 7000);
 
     toChatList()
 }
